dba9c2ea874535b93b8a715d9c8931db4e391d43
wallacesintra <wallvxwallace962@gmail.com>
2024-02-09 21:00:21 +0300

    Add cmd.Cmd class and implement greet, multiply, and add commands

--- reference/README.md
+++ reference/README.md
@@ -3,5 +3,11 @@ cmd module
 Cmd class provides a simple framework for writing line-oriented command interpreters.
 
  class cmd.Cmd(completekey='tab', stdin=None, stdout=None)
-    use it as a super class.
-    
+    use it as a super class
+
+
+
+UUID - Universal Unique Identifier
+-----------------------------------
+128 bit
+
--- reference/my_cmd.py
+++ reference/my_cmd.py
@@ -1,17 +1,42 @@
 #!/usr/bin/env python3
 import cmd
-# from cmd import Cmd
+### This is a simple example of a command line interface using the cmd module
+
+def multiply(line):
+    arr = line.split()
+    product = 0
+
+    for i in arr:
+        product *= int(i)
+    return product
 
 class HelloWorld(cmd.Cmd):
     """Simple command processor example."""
+    prompt = "(hbnb)"
     def do_greet(self, line):
-        print("Hello")
-
-    def do_greetName(self, line):
         if line:
-            print("Hi {}".format(line))
+            print("Hi, {}".format(line))
         else:
             print("Hi ")
+
+    def do_multiply(self, line):
+        product = multiply(line)
+        print(product)
+
+    def do_add(self, line):
+        """Add numbers together"""
+        arr = line.split()
+        sum = 0
+        if len(arr) < 2:
+            print("Usage: add <number1> <number2> ...")
+            return
+        else:
+            try:
+                for i in arr:
+                    sum += int(i)
+                print(sum)
+            except ValueError:
+                print("Usage: add <number1> <number2> ...")
 
     def do_EOF(self,line):
         return True
